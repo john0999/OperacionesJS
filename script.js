ad3afc12ee419361c22facb3d5995881651c712b
john0999 <moralesgomezjohnny@gmail.com>
2024-06-12 12:26:44 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function sumar() {
+    try {
+        let num1 = parseInt(document.getElementById("valor1").value);
+        let num2 = parseInt(document.getElementById("valor2").value);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('Por favor rellena los campos e ingresa números válidos.');
+            return;
+        }
+
+        let suma = num1 + num2;
+        document.form.resultado.value = suma;
+    } catch (error) {
+        alert('ERROR DE CÓDIGO: ' + error);
+        return;
+    }
+}
+
+function restar() {
+    try {
+        let num1 = parseInt(document.getElementById("valor1").value);
+        let num2 = parseInt(document.getElementById("valor2").value);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('Por favor rellena los campos e ingresa números válidos.');
+            return;
+        }
+
+        let resta = num1 - num2;
+        document.form.resultado.value = resta;
+    } catch (error) {
+        alert('ERROR DE CÓDIGO: ' + error);
+        return;
+    }
+}
