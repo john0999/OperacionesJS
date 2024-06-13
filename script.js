744f71212b37b689f53be736249bf69d32aec476
john0999 <moralesgomezjohnny@gmail.com>
2024-06-13 09:59:38 -0600

    Multiplicar y Dividir

--- script.js
+++ script.js
@@ -33,3 +33,39 @@ function restar() {
         return;
     }
 }
+
+function multiplicar() {
+    try {
+        let num1 = parseInt(document.getElementById("valor1").value);
+        let num2 = parseInt(document.getElementById("valor2").value);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('Por favor rellena los campos e ingresa números válidos.');
+            return;
+        }
+
+        let multiplicacion = num1 * num2;
+        document.form.resultado.value = multiplicacion;
+    } catch (error) {
+        alert('ERROR DE CÓDIGO: ' + error);
+        return;
+    }
+}
+
+function dividir() {
+    try {
+        let num1 = parseInt(document.getElementById("valor1").value);
+        let num2 = parseInt(document.getElementById("valor2").value);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            alert('Por favor rellena los campos e ingresa números válidos.');
+            return;
+        }
+
+        let division = num1 / num2;
+        document.form.resultado.value = division;
+    } catch (error) {
+        alert('ERROR DE CÓDIGO: ' + error);
+        return;
+    }
+}
